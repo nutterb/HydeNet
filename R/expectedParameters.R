2a3be9275e69554fb21f32810ea06c361bea5df6
nutterb <benjamin.nutter@gmail.com>
2015-02-05 17:09:18 -0500

    Various updates preparing for vignette

--- R/expectedParameters.R
+++ R/expectedParameters.R
@@ -4,9 +4,11 @@ expectedParameters <- function(network, node, returnVector=FALSE){
   node <- as.character(substitute(node))
   inputs <- network$nodeType[[node]]
   
+#   return(list(node, inputs))
+  
 #   data(jagsDists, package='Hyde')
   params <- jagsDists$Parameters[jagsDists$FnName == inputs]
-  
+
   if (returnVector) return(params)
   else cat(paste("c(", paste(paste0(params, "= "), collapse=", "), ")"))
 }
--- R/setNode.R
+++ R/setNode.R
@@ -6,30 +6,30 @@
 #'   before the appropriate JAGS model statement can be constructed.  
 #'   \code{setNode} is the utility by which a user can define the distribution
 #'   of the node and its relationship to its parents (usually through a model
-#'   of some sort).  
+#'   of some sort).
 #'   
 #' @param network A \code{HydeNetwork}.
 #' @param node A node within \code{network}.  This does not have to be quoted.
 #' @param nodeType a valid distribution function from JAGS.  See the data set
 #'   in \code{data(jagsDists)} for a complete list.
-#' @param params to be passed to the JAGS distribution function.  Each parameter
-#'   in the distribution function must be a named element in the vector.  For 
-#'   example, the parameters to pass to \code{dnorm} would be \code{c(mu='', tau='')}.
-#'   The required parameters can be looked up using the 
-#'   \code{expectedParameters} function.  If parameters are to be estimated 
-#'   from the data, the functions \code{fromData} and \code{fromFormula} may 
-#'   be used as placeholders.
 #' @param nodeFitter the fitting function, such as \code{lm} or \code{glm}.  This
 #'   will probably only be needed when \code{fromData = TRUE}.
 #' @param nodeFormula A formula object specifying the relationship between a 
 #'   node and its parents.  It must use as a term every parent of \code{node}.
+#' @param fitterArgs Additional arguments to be passed to \code{fitter}.  This does not 
+#'   yet have any effect as I haven't yet decided out where to store this and 
+#'   how to implement the fitting.
 #' @param fromData Logical.  Determines if a node's relationship is calculated 
 #'   from the data object in \code{network}.  Defaults to \code{TRUE} whenever
 #'   \code{network} has a data object.
-#' @param ... Additional arguments to be passed to \code{fitter}.  This does not 
-#'   yet have any effect as I haven't yet decided out where to store this and 
-#'   how to implement the fitting.
-#'   
+#' @param ... parameters to be passed to the JAGS distribution function.  Each parameter
+#'   in the distribution function must be named.  For 
+#'   example, the parameters to pass to \code{dnorm} would be \code{mu='', tau=''}.
+#'   The required parameters can be looked up using the 
+#'   \code{expectedParameters} function.  If parameters are to be estimated 
+#'   from the data, the functions \code{fromData} and \code{fromFormula} may 
+#'   be used as placeholders.
+#'   #'   
 #' @details \code{HydeNetwork} doesn't create a space for the \code{params} to be
 #'   stored.  I put this in place in case a non-gaussian distribution was desired
 #'   that did not lend itself to a modeling estimation.  I don't know what exactly
@@ -74,9 +74,11 @@
 #' carNet$nodeFitter$mpg
 #' carNet$nodeType$mpg
 
-setNode <- function(network, node, nodeType='dnorm', params=NULL, 
-                    nodeFitter=NULL, nodeFormula, 
+setNode <- function(network, node, nodeType, 
+                    nodeFitter, nodeFormula, 
+                    fitterArgs = list(),
                     fromData=!is.null(network$data), ...){
+  
   network.t <- as.character(substitute(network))
   node.t <- as.character(substitute(node))
   
@@ -88,19 +90,14 @@ setNode <- function(network, node, nodeType='dnorm', params=NULL,
   wrn.flag <- 0
   wrn.flag <- ""
   
-  #*****************************
-  #* nodeType parameter checks
-  if (missing(nodeType)){
-    err.flag <- err.flag + 1
-    err.msg <- c(err.msg, 
-                 paste0(err.flag, ": nodeType is not specified."))
-  }
   
-  if (length(nodeType) > 1){
-    wrn.flag <- wrn.flag + 1
-    wrn.msg <- c(wrn.msg,
-                 paste0(wrn.flag, ": nodeType must have length 1. The first element is being used."))
-    nodeType <- nodeType[1]
+  if (!missing(nodeType)){
+    if (length(nodeType) > 1){
+      wrn.flag <- wrn.flag + 1
+      wrn.msg <- c(wrn.msg,
+                   paste0(wrn.flag, ": nodeType must have length 1. The first element is being used."))
+      nodeType <- nodeType[1]
+    }
   }
   
   if (!missing(nodeType)){
@@ -112,33 +109,25 @@ setNode <- function(network, node, nodeType='dnorm', params=NULL,
     }
   }
 
-  # translate inputted parameters into JAGS code if the params argument was not specified
-  if (missing(params)){
-    
-    inputtedArgs <- ls()
-    
-    if(nodeType=="dbern"){
-      if("pi" %in% inputtedArgs){
-        if(!is.numeric(pi)) {
-          err.flag <- err.flag + 1
-          err.msg  <- c(err.msg, paste0(err.flag, ": dbern must have 0 <= pi <= 1"))
-        } else if(pi<0 | pi>1){
-          err.flag <- err.flag + 1
-          err.msg  <- c(err.msg, paste0(err.flag, ": dbern must have 0 <= pi <= 1"))
-        } else params <- paste0("pi."deparse(substitute(node)) <- ",pi,";")
-      } else {
-        err.flag <- err.flag + 1
-        err.msg  <- c(err.msg, paste0(err.flag, ": parameters missing for dbern.  See help(\"setNode\")."))
-      }
-    }
-    
+  if (!missing(nodeType)) network$nodeType[[node.t]] <- nodeType
+ 
+  exp_param <- eval(substitute(expectedParameters(network, node, TRUE)))
+  params <- list(...)[exp_param]
+
+  if (!all(exp_param %in% names(params))){
+    err.flag <- err.flag + 1
+    err.msg <- c(err.msg,
+                 paste0(err.flag, ": Nodes of type ", network$nodeType[[node.t]], 
+                        " must have all of the following parameters--",
+                        paste(exp_param, collapse=", "), "."), collapse="\n")
   }
-  
-  network$nodeType[[node.t]] <- nodeType
-  network$nodeParams[[node.t]] <- params
+
+  if (length(list(...))) network$nodeParams[[node.t]] <- list(...)
   if (!missing(nodeFormula)) network$nodeFormula[[node.t]] <- nodeFormula
-  network$nodeFitter[[node.t]] <- nodeFitter
-  network$nodeFitterArgs[[node.t]] <- list(...)
+  if (!missing(nodeFitter)) network$nodeFitter[[node.t]] <- nodeFitter
+  if (length(fitterArgs)) network$nodeFitterArgs[[node.t]] <- fitterArgs
   
+
+  if (err.flag) stop(paste(err.msg, collapse="\n"))
   return(network)  
 }
