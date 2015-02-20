207357ab50aba1d80562c090a217895feca1e5bb
nutterb <benjamin.nutter@gmail.com>
2015-02-20 09:01:32 -0500

    Data Documentation

---
+++ R/PEData.R
@@ -0,0 +1,16 @@
+#' PE Data Set (Better Title)
+#'
+#' Description of Data Set
+#'
+#' @format A data frame with 10000 rows and 7 variables:
+#' \describe{
+#'   \item{wells}{}
+#'   \item{pregnant}{}
+#'   \item{pe}{}
+#'   \item{angio}{}
+#'   \item{d.dimer}{}
+#'   \item{treat}{}
+#'   \item{death}{}
+#' }
+#' @source WHAT IS THE SOURCE?
+"PE"
