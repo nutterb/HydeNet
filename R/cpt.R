7a3cbe029f514b82d19c0d351c3b3ea450e28b75
jdutch27 <daltonj@Jarrods-MacBook-Pro.local>
2015-05-16 06:34:47 -0400

    add list input functionality to vars param

--- R/cpt.R
+++ R/cpt.R
@@ -6,10 +6,19 @@
 #'   it computes the conditional probability distribution of one of the factors
 #'   given other factors, and stores the result in a multidimensional \code{array}. 
 #'   
-#' @param formula a formula with the following structure: \emph{response ~ var1 + var2 + ...}
+#' @param vars either a formula or a list containing the names of the variables 
+#'   used to compute the conditional probability table. See details.
 #' @param data a data frame containing all the factors represented by the \code{formula}
 #'   parameter.
 #' @param wt (optional) a numeric vector of observation weights.
+#' 
+#' @details If a \code{formula} object is entered for the \code{vars} parameter, the
+#'   formula must have the following structure: \emph{response ~ var1 + var2 + etc.}.
+#'   The other option is to pass a named \code{list} containing two elements \code{y}
+#'   and \code{x}. Element \code{y} is a character string containing the name of the 
+#'   factor variable in \code{data} to be used as the dependent variable, and 
+#'   element \code{x} is a character vector containing the name(s) of the factor
+#'   variable(s) to be used as independent (or conditioning) variables.
 #'   
 #' @author Jarrod Dalton and Benjamin Nutter
 #' 
@@ -18,35 +27,74 @@
 #' 
 #' n <- 50000 
 #' data <- data.frame(
-#' di1=as.factor(1:6 %*% rmultinom(n,1,prob=c(.4,.3,.15,.10,.03,.02))),
-#' di2=as.factor(1:6 %*% rmultinom(n,1,prob=rev(c(.4,.3,.15,.10,.03,.02)))),
-#' di3=as.factor(1:6 %*% rmultinom(n,1,prob=c(.15,.10,.02,.3,.4,.03)))
+#'   di1 = as.factor(1:6 %*% rmultinom(n,1,prob=c(.4,.3,.15,.10,.03,.02))),
+#'   di2 = as.factor(1:6 %*% rmultinom(n,1,prob=rev(c(.4,.3,.15,.10,.03,.02)))),
+#'   di3 = as.factor(1:6 %*% rmultinom(n,1,prob=c(.15,.10,.02,.3,.4,.03)))
 #' )
-#' cpt <- cpt(di3 ~ di1 + di2, data)
-#' plyr::aaply(cpt, c(1,2), sum)
-#'
+#' 
+#' cpt1 <- cpt(di3 ~ di1 + di2, data)
+#' plyr::aaply(cpt1, c(1,2), sum)
+#' 
+#' l <- list(y = "di3", x = c("di1","di2"))
+#' all(cpt(l, data) == cpt1)
 
 
-cpt <- function(formula, data, wt){
+cpt <- function(vars, data, wt){
   err.flag <- 0
   err.msg <- ""
   
   wrn.flag <- 0
   wrn.msg <- ""
   
-  if(!"formula" %in% class(formula)){
+  if("formula" %in% class(vars)){
+    
+    variables       <- as.character(attr(terms(vars), "variables"))[-1]
+    dependentVar    <- variables[1]
+    independentVars <- variables[-1]
+    
+  } else if("list" %in% class(vars)){
+    
+    if(!all(c("y","x") %in% names(vars))){
+      err.flag <- err.flag + 1
+      err.msg <- c(err.msg, paste0(err.flag,
+                                   ": List object 'vars' must contain character vectors ",
+                                   "'y' and 'x'. See help('cpt')."))
+    }
+    
+    if(!all(unlist(lapply(vars,is.character)))){
+      err.flag <- err.flag + 1
+      err.msg <- c(err.msg, paste0(err.flag,
+                                   ": List object 'vars' must contain character vectors ",
+                                   "'y' and 'x'. See help('cpt')."))
+    }
+    
+    if(length(vars[["y"]]) != 1){
+      err.flag <- err.flag + 1
+      err.msg <- c(err.msg, paste0(err.flag,
+                                   ": Element 'y' of list object 'vars' must be a character ",
+                                   "vector of length 1. See help('cpt')."))
+    }
+    
+    variables       <- c(vars[["y"]], vars[["x"]])
+    dependentVar    <- vars[["y"]]
+    independentVars <- vars[["x"]]
+    
+  } else {
+    
     err.flag <- err.flag + 1
-    err.msg <- c(err.msg, paste0(err.flag,": Object 'formula' must be of class 'formula'"))
+    err.msg <- c(err.msg, paste0(err.flag,
+                                 ": Parameter 'vars' must be either a formula or list. ",
+                                 "See help('cpt')."))
+    
   }
-  variables <- as.character(attr(terms(formula), "variables"))[-1]
-  dependentVar <- variables[1]
-  independentVars <- variables[-1]
   
   if(!is.data.frame(data)){
     err.flag <- err.flag + 1
     err.msg <- c(err.msg, paste0(err.flag,": Object 'data' must be of class 'data.frame'"))
   }
   n <- nrow(data)
+  
+  if(err.flag)  stop(paste(err.msg, collapse="\n"))
   
   missingVariables <- which(!variables %in% names(data))
   if(length(missingVariables)>0){
@@ -62,7 +110,7 @@ cpt <- function(formula, data, wt){
   
   if(!all(unlist(lapply(data[,variables],function(x) "factor" %in% class(x))))){
     err.flag <- err.flag + 1
-    err.msg <- c(err.msg, paste0(err.flag, ": All variables in 'formula' must be of class 'factor'"))
+    err.msg <- c(err.msg, paste0(err.flag, ": All variables must be of class 'factor'"))
   }
   
   if(missing(wt)) wt <- rep(1,n) else {
